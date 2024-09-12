d0f56f399ffa71e74ab1ceff78b522a394037da6
Aatik Ali Makani <aatikali99@gmail.com>
2024-09-11 23:35:50 -0500

    Update assign1.py

--- homeworks/assign1.py
+++ homeworks/assign1.py
@@ -1,24 +1,24 @@
-# # import code 
+# import code 
 
-# #EXERCISE 1
+#EXERCISE 1
 
-# def remove_whitespace (word):
-#     return word.replace(" ", "")
+def remove_whitespace (word):
+    return word.replace(" ", "")
 
-# assert remove_whitespace("Linus Torvalds") == "LinusTorvalds"
-# assert remove_whitespace("n vidia is aw ful") == "nvidiaisawful"
-# assert remove_whitespace("i don't like mentos") == "idon'tlikementos"
+assert remove_whitespace("Linus Torvalds") == "LinusTorvalds"
+assert remove_whitespace("n vidia is aw ful") == "nvidiaisawful"
+assert remove_whitespace("i don't like mentos") == "idon'tlikementos"
 
-# def assert_short_name (name):
-#     assert len(name)<6, "name is too long"
-#     return f"{name} is a short name"
+def assert_short_name (name):
+    assert len(name)<6, "name is too long"
+    return f"{name} is a short name"
 
 
-# def average_positives (list):
-#     for num in list:
-#         assert num > 0, "not_a positive value"
-#     average = sum(list)/len(list)
-#     return average
+def average_positives (list):
+    for num in list:
+        assert num > 0, "not_a positive value"
+    average = sum(list)/len(list)
+    return average
 
 
 # EXERCISE 2
